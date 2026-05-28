11609c588b3e4e3567df79a11446695107793efd
KomalYadav <2024.komal.yadav@ves.ac.in>
2026-05-28 20:24:11 +0530

    Fix: Allow Vercel production origin in backend CORS config

--- Expense_Tracker_Backend/src/main/java/com/example/ExpenseTracker2/Security/SecurityConfig.java
+++ Expense_Tracker_Backend/src/main/java/com/example/ExpenseTracker2/Security/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000","https://expense-tracker-frontend-three-wine.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
